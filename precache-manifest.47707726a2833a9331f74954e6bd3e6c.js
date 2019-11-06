8fe76e1a1996c0da430c4c515ce303392cecc944
Anastasiia Pavlenko <anastasia2ustimets@gmail.com>
2019-11-07 00:48:56 +0200

    Updates

---
+++ precache-manifest.47707726a2833a9331f74954e6bd3e6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6aaf3da039aeab8ba910f6fe3c2463",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "f906d324d6c3599934ae",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d8509f3d27cec7aa7df9",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.5ba4facc.chunk.css"
+  },
+  {
+    "revision": "f906d324d6c3599934ae",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.0084d703.chunk.js"
+  },
+  {
+    "revision": "d8509f3d27cec7aa7df9",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.b2e0d814.chunk.js"
+  },
+  {
+    "revision": "9442ab920daa0b9f3f5e",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.8518e471.js"
+  }
+]);
